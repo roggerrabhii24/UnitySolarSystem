0321dbb52a9a7e5aff76c627dff38dcd57c2e8ca
Abhishek <abhisheknewaskar@icloud.com>
2026-04-28 23:07:37 +0530

    planet exploration

--- Assets/PlanetSceneLoader.cs
+++ Assets/PlanetSceneLoader.cs
@@ -6,12 +6,25 @@ public class PlanetSceneLoader : MonoBehaviour
 
     public void OnPlanetSelected(int index)
     {
-        if (index == 1) // Earth
+        // Dropdown order:
+        // 0 = Solar System
+        // 1 = Earth
+        // 2 = Moon
+        // 3 = Mars
+
+        if (index == 0)
+        {
+            LoadSolarSystem();
+        }
+        else if (index == 1)
         {
             transitionManager.LoadSceneWithFade("EarthExperience");
         }
-
-        if (index == 2) // Mars
+        else if (index == 2)
+        {
+            transitionManager.LoadSceneWithFade("MoonLunarLand");
+        }
+        else if (index == 3)
         {
             transitionManager.LoadSceneWithFade("Mars Landscape 3D Overview");
         }
